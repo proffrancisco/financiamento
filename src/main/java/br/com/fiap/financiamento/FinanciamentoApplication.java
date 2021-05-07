8357a471f4fc3b976a6f462efe354eefd1410bdb
unknown <Administrator@L2501MICRO37.fiap.com.br>
2021-05-07 16:57:46 -0300

    primeiro commit do projeto

---
+++ src/main/java/br/com/fiap/financiamento/FinanciamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.financiamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanciamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/financiamento/FinanciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.financiamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
